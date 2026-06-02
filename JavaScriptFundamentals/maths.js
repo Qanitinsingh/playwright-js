14d46d4b056a050af51443f2a8742ea8db754771
nitinkumar <qanitin1@gmail.com>
2026-06-02 10:43:21 +0530

    added math.js to explain different variations using js

---
+++ JavaScriptFundamentals/maths.js
@@ -0,0 +1,45 @@
+// maths.js
+
+// Input Variables
+let num1 = 20;
+let num2 = 10;
+
+console.log("Number 1 :", num1, "| Type :", typeof num1);
+console.log("Number 2 :", num2, "| Type :", typeof num2);
+
+// Addition
+let addition = num1 + num2;
+console.log("Addition :", addition);
+
+// Subtraction
+let subtraction = num1 - num2;
+console.log("Subtraction :", subtraction);
+
+// Multiplication
+let multiplication = num1 * num2;
+console.log("Multiplication :", multiplication);
+
+// Division
+let division = num1 / num2;
+console.log("Division :", division);
+
+// Modulus (Remainder)
+let remainder = num1 % num2;
+console.log("Remainder :", remainder);
+
+// Increment
+let count = 5;
+count++;
+console.log("Increment :", count);
+
+// Decrement
+count--;
+console.log("Decrement :", count);
+
+// Square
+let square = num1 * num1;
+console.log("Square :", square);
+
+// Average
+let average = (num1 + num2) / 2;
+console.log("Average :", average);
